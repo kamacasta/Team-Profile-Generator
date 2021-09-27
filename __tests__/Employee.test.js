3bb0bee6f5290228eb7ca9b7cf8dbea367ad9b41
kama casta <kamacasta983@gmail.com>
2021-09-26 20:59:42 -0700

    running into bugs

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,5 +3,30 @@ const Employee = require("../lib/Employee.js")
 
 test('creates employee object', () => 
 {
-    const employee = new Employee("loremintino",)
+    const employee = new Employee("kamakanui", 131822, "kamacasta983@gmail.com");
+    expect(typeof employee).toBe("object");
+
+    // expect a String or Number based on the property
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+// tests for name 
+test("add name and getName method will return an employee's name", () => {
+   
+    // const name = "Kamakanui"
+    const employee = new Employee("kamakanui", 131822, "kamacasta983@gmail.com");
+    expect(employee.getName()).toBe(expect.any(String));
 })
+
+// tests for id 
+test("add id and getId method will return an employee's id", () => {
+    
+    // const id = "131822";
+    const employee = new Employee("kamakanui", 131822, "kamacasta983@gmail.com");
+    expect(employee.getId()).toEqual(expect.any(Number));
+    expect(employee.getId()).toEqual(id);
+})
+
+
