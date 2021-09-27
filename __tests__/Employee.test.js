dd89a108aa718d8500955c2d704de6aa46761fd4
kama casta <kamacasta983@gmail.com>
2021-09-26 19:02:48 -0700

    tests ran and failed now creating constructor and now working on more test logic

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,7 @@
+const { expect } = require("@jest/globals");
+const Employee = require("../lib/Employee.js")
+
+test('creates employee object', () => 
+{
+    const employee = new Employee("loremintino",)
+})
