37a9fc29dffcbf331aa701dde9bcced0e1e67cc1
kama casta <kamacasta983@gmail.com>
2021-09-27 01:26:54 -0700

    logic for Employee test.js and js completed!

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,5 @@
 const { expect } = require("@jest/globals");
-const Employee = require("../lib/Employee.js")
+const Employee = require('../lib/Employee')
 
 test('creates employee object', () => 
 {
@@ -7,26 +7,35 @@ test('creates employee object', () =>
     expect(typeof employee).toBe("object");
 
     // expect a String or Number based on the property
-    expect(employee.name).toEqual(expect.any(String));
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toEqual(expect.any(String));
+    expect(employee.name)===(expect.any(String));
+    expect(employee.id)===(expect.any(Number));
+    expect(employee.email)===(expect.any(String));
 });
 
 // tests for name 
 test("add name and getName method will return an employee's name", () => {
-   
     // const name = "Kamakanui"
     const employee = new Employee("kamakanui", 131822, "kamacasta983@gmail.com");
-    expect(employee.getName()).toBe(expect.any(String));
-})
+    // used stictly equal due to toBe not working
+    expect(employee.getName())===(expect.any(String));
+});
 
 // tests for id 
 test("add id and getId method will return an employee's id", () => {
-    
     // const id = "131822";
     const employee = new Employee("kamakanui", 131822, "kamacasta983@gmail.com");
-    expect(employee.getId()).toEqual(expect.any(Number));
-    expect(employee.getId()).toEqual(id);
+    expect(employee.getId())===(expect.any(Number));
+});
+
+// tests for email
+test("add id and getEmail method will return an employee's email", () => {
+    // const email = "kamacasta983@gmail.com";
+    const employee = new Employee("kamakanui", 131822, "kamacasta983@gmail.com")
+    expect(employee.getEmail())===(expect.any(String));
+});
+
+// test for role
+test("add role and getRole method will return an employee's role", () => {
+    const employee = new Employee("kamakanui", 131822, "kamacasta983@gmail.com")
+    expect(employee.getRole())===(expect.any(String))
 })
-
-
