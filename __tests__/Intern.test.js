f9a8a9fe668cecb4924277942076239bcd54fa60
kama casta <kamacasta983@gmail.com>
2021-09-27 02:38:42 -0700

    test passed

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -5,10 +5,27 @@ test('create intern object', () => {
     const intern = new Intern();
     expect(typeof intern).toBe("object");
 
-    expect(engineer.name)===(expect.any(String));
-    expect(engineer.id)===(expect.any(Number));
-    expect(engineer.email)===(expect.any(String));
-    expect(engineer.school)===(expect.any(String));
+    expect(intern.name)===(expect.any(String));
+    expect(intern.id)===(expect.any(Number));
+    expect(intern.email)===(expect.any(String));
+    expect(intern.school)===(expect.any(String));
 });
+
+// test for school
+test("add name and getSchool method will return an intern's school", () => 
+{
+    const school = "University of Arizona"
+    const intern = new Intern("kj", 92480, "kjcasta983@gmail.com", school);
+
+    expect(intern.getSchool())===(school);
+})
+
+// test for role
+test("add name and getRole method will return an intern's role", () => 
+{
+    const role = "Intern";
+    const intern = new Intern("kj", 92480,"kjcasta983@gmail.com", role)
+    expect(intern.getRole()===(role));
+})
 
 
